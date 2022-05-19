4c519d0c9133c1086cb8d5eaec1c3fd6ce831ab0
JannyAndrade <97898121+JannyAndrade@users.noreply.github.com>
2022-05-19 20:22:53 -0300

    Add files via upload

---
+++ exe038.py
@@ -0,0 +1,12 @@
+n1 = int(input('Digite um numero:'))
+n2 = int(input('Digite um numero:'))
+if n1 > n2:
+    print('O primeiro numero é maior')
+
+elif n2 > n1:
+    print('O segundo numero é maior ')
+
+else:
+    print('Os numeros sao iguais ')
+
+
